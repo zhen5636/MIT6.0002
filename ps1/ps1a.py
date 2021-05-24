20f8da717b7f3902ec750e9a934a7c72e7766917
lcsm29 <lcsm29@outlook.com>
2021-05-25 02:15:54 +0900

    added Name, Collaborator, Time spent

--- ps1/ps1a.py
+++ ps1/ps1a.py
@@ -1,8 +1,8 @@
 ###########################
 # 6.0002 Problem Set 1a: Space Cows 
-# Name:
-# Collaborators:
-# Time:
+# Name: lcsm29
+# Collaborators: None
+# Time spent (hh:mm): 01:05
 
 from ps1_partition import get_partitions
 import time
